4bb1dae77caf92f0d8c8b3bd518cb72cc62500ca
hasanrafi69 <hasanrafi69@gmail.com>
2022-01-26 22:08:22 +0600

    learning basic js

---
+++ array.js
@@ -0,0 +1,10 @@
+var friendsAge = [11, 12, 35, 55, 34];
+//=======length======// count number of data in array
+// friendsAge = friendsAge.length;
+//=======indexOf()======// findout position of data in array
+// friendsAge = friendsAge.indexOf(55);
+//=======push()======// Add data into array at the last position
+// friendsAge.push(33);
+// ======pop()=======// remove data from array last position
+// friendsAge.pop();
+console.log(friendsAge);
---
+++ basic.js
@@ -0,0 +1,14 @@
+var price = 22;
+var price2 = 224;
+var strings = "22"
+var sum = price + price2;
+var mul = price * price2;
+var div = price2 / price;
+var des = price2 - price;
+var constrings = parseInt(strings);
+
+console.log(sum, mul, div, des, constrings);
+
+var num1 = "99.5";
+var num2 = 15;
+console.log(num1 + num2);
