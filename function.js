7d6e45f00b56cd91a6373f8901c406f7beda8953
hasanrafi69 <hasanrafi69@gmail.com>
2022-01-30 13:26:20 +0600

    leap year

--- function.js
+++ function.js
@@ -65,3 +65,18 @@ var inputName = 'Hasan' // Change this name
 var inputNumber = 88; // Change this number 
 var result = grade(inputName, inputNumber);
 console.log(result);
+
+
+// ==========Get Yeas isA lepyear or NOt===========//
+//=================================================//
+function leapYear(year) {
+    if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
+        return decision = 'Yes, ' + year + ' is a leapyear';
+    } else {
+        return decision = 'Yes, ' + year + ' is not a leapyear';
+    }
+
+}
+var input = 2016;
+var result = leapYear(input);
+console.log(result);
