f77e356b44ffe7b194f3355c449f29f4ea3e510e
hasanrafi69 <hasanrafi69@gmail.com>
2022-01-31 00:01:32 +0600

    fixed food delivery function

--- function.js
+++ function.js
@@ -98,12 +98,12 @@ console.log(result);
 // ========Simple Food Order Confirmation==========//
 //=================================================//
 function foodDelivery(foodname, quantity, price, discountpar) {
-    if (foodname.toLowerCase() == 'pizza') {
+    if (foodname.toLowerCase() == 'pizza' && quantity <= 30 && quantity >= 1) { // if quantity is gatter than 30 & minium 1
         if (quantity > 10) {
             var total = quantity * price;
             var discount = total * discountpar;
             var discountPrice = total - discount;
-            var message = foodname + ' Order Placed Successfully' + ' Your price is' + ' $' + discountPrice;
+            var message = foodname + ' Order Placed Successfully' + ' Your Discount price is' + ' $' + discountPrice;
             return message;
         } else {
             var total = quantity * price;
@@ -111,15 +111,21 @@ function foodDelivery(foodname, quantity, price, discountpar) {
             return message;
         }
 
+    } else if (quantity > 30) {
+        message = 'Sorry! Out of Stock';
+        return message;
+    } else if (quantity <= 0) {
+        message = 'Sorry Please Order minimum atleast one';
+        return message;
     } else {
         message = 'Sorry We only sale pizza';
         return message;
     }
 }
 // Change this value
-var foodName = 'Pizza'; // Give Food Name
-var foodPrice = 80; // give Price
-var foodQuantity = 11; // Give quantity you want, if quantity is more than 10, you will get 10% off
+var foodName = 'pIzza'; // Give Food Name
+var foodPrice = 40; // give Price
+var foodQuantity = 10; // Give quantity you want, if quantity is more than 10, you will get 10% off
 var discountParcent = 10 / 100; // How much give you off as percnet ( this 10% example)
 var result = foodDelivery(foodName, foodQuantity, foodPrice, discountParcent);
 console.log(result);
